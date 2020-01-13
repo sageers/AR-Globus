8dea44b129c245fdd68127146f74e8fae8772917
Kylors <solomonius@web.de>
2020-01-13 02:01:30 +0100

    Merge
    Merge von Zoom und dem neuen 3D-Model des Globus. Highlighting von ausgewählten Landmassen. Tippen auf die Landmasse nötig für eine Skalierung/Fokussierung. Material-Änderung verbessert.

---
+++ AR-Globus-Zoom_Model_Merge/AR Test/Assets/MoveAllObjects.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAllObjects : MonoBehaviour
+{
+
+    
+    public static List<GameObject> objectsOnEarth = new List<GameObject>();
+
+    public static bool move;
+
+    public void toggleObjects()
+    {
+        move = !move;
+    }
+
+//    public void showAllObjects()
+//    {
+//        
+//        
+//        foreach (var movableObject in objectsOnEarth)
+//        {
+//            movableObject.GetComponent<move>().MoveObject();
+//        }
+//    }
+    
+}
