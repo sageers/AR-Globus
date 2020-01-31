a12a2f3fa9d819b07cead394d6a6b91677361e96
Sarah S <sageer.soe@gmail.com>
2020-01-31 16:54:58 +0100

    UI Logik für Quest Modus
    Basic UI Logik. Quest Modus starten, Random Frage auswählen, zur nächsten Fragen springen

--- AR Test/Assets/Scripts/QuestModeUIEvents.cs
+++ AR Test/Assets/Scripts/QuestModeUIEvents.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Experimental.UIElements;
 using Vuforia;
+using Button = UnityEngine.UI.Button;
+using Random = System.Random;
 
 public class QuestModeUIEvents : MonoBehaviour
 {
@@ -13,10 +17,40 @@ public class QuestModeUIEvents : MonoBehaviour
    public GameObject panel_Frage;
    public GameObject panel_Frage_klein;
    public Button btn_Frage_weiter;
-   
+
+   public TextAsset questQuestion;
    
    private bool frage_weiter;
 
+   
+   protected FileInfo fileQuestions = null;
+   protected StreamReader reader = null;
+   protected string QuestionText = " ";
+   public int AnzahlmöglicherFragen;
+   private bool coroutineRunning = true;
+   
+   
+   void Awake () {
+      
+      
+      fileQuestions = new FileInfo (@"Assets\Resources\FragenQuestModus.txt");
+      //print(fileQuestions);
+      reader = fileQuestions.OpenText();
+      
+   }
+   
+   void Start () {
+      
+      
+      fileQuestions = new FileInfo (@"Assets\Resources\FragenQuestModus.txt");
+      //print(fileQuestions);
+      reader = fileQuestions.OpenText();
+      
+   }
+
+   
+   
+   
    public void toggleFrage()
    {
       frage_weiter = !frage_weiter;
@@ -24,34 +58,118 @@ public class QuestModeUIEvents : MonoBehaviour
 
    public void newQ()
    {
+      coroutineRunning = true;
+      //StopAllCoroutines();
       StartCoroutine(showNewQuestion());
    }
-   
+
+   public void nextQ()
+   {
+      coroutineRunning = true;
+      StartCoroutine(nextQuestion());
+   }
+
+  
    public IEnumerator showNewQuestion()
    {
-      panel_Frage_klein.SetActive(false);
-      panel_Frage.SetActive(true);
-      txt_Frage.GetComponent<TextMeshProUGUI>().text = "Get random Question out of Question-catalogue";
-      txt_Frage_klein.GetComponent<TextMeshProUGUI>().text = "Get random Question out of Question-catalogue";
-
-      var counter = 0;
-      while (!frage_weiter)
-      {
-         yield return new WaitForSeconds(1);
-         counter++;
-
-         if (counter > 20)
-         {
-            break;
-         }
-      }
-
-      frage_weiter = false;
       
-      panel_Frage.SetActive(false);
-      panel_Frage_klein.SetActive(true);
+      while (coroutineRunning)
+      {
+         
+         panel_Frage_klein.SetActive(false);
+         btn_Frage_weiter.gameObject.SetActive(false);
+         
+         var rand = UnityEngine.Random.Range(1, AnzahlmöglicherFragen + 1);
+         print(rand);
+         QuestionText = chooseQuestion(rand);
+         
+         panel_Frage.SetActive(true);
+         txt_Frage.GetComponent<TextMeshProUGUI>().text = QuestionText;
+         txt_Frage_klein.GetComponent<TextMeshProUGUI>().text = QuestionText;
+         
+         
+         var counter = 0;
+         while (!frage_weiter)
+         {
+            yield return new WaitForSeconds(1);
+            counter++;
+   
+            if (counter > 20)
+            {
+               break;
+            }
+         }
+         btn_Frage_weiter.gameObject.SetActive(true);
+         frage_weiter = false;
+         
+         panel_Frage.SetActive(false);
+         panel_Frage_klein.SetActive(true);
+
+         coroutineRunning = false;
+         yield return null;
+      }
+      
       
       
       //Frage nach oben links
+   }
+
+   public IEnumerator nextQuestion()
+   {
+      while (coroutineRunning)
+      {
+         
+         coroutineRunning = false;
+         panel_Frage_klein.SetActive(false);
+         btn_Frage_weiter.gameObject.SetActive(false);
+         
+         var rand = UnityEngine.Random.Range(1, AnzahlmöglicherFragen + 1);
+         print(rand);
+         QuestionText = chooseQuestion(rand);
+               
+         panel_Frage.SetActive(true);
+         txt_Frage.GetComponent<TextMeshProUGUI>().text = QuestionText;
+         txt_Frage_klein.GetComponent<TextMeshProUGUI>().text = QuestionText;
+         
+               
+         var counter = 0;
+         while (!frage_weiter)
+         {
+            yield return new WaitForSeconds(1);
+            counter++;
+         
+            if (counter > 20)
+            {
+               break;
+            }
+         }
+         btn_Frage_weiter.gameObject.SetActive(true);
+         frage_weiter = false;
+               
+         panel_Frage.SetActive(false);
+         panel_Frage_klein.SetActive(true);
+
+         
+         yield return null;
+      }
+   }
+   
+   private String chooseQuestion(int number)
+   {
+      
+      var textTemp = "";
+      
+      for (int i = 0; i < number; i++)
+      {
+         
+         textTemp = reader.ReadLine();
+         
+      }
+         reader.Close();
+         reader = fileQuestions.OpenText();
+         //QuestionText = textTemp;
+        
+      
+      return textTemp;
    }
 }
